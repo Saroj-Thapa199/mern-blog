b1b478ff0523aba7a127c4cd8d922bc248da3287
Mystic999 <eyesd676@gmail.com>
2024-07-16 11:31:37 +0545

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
-const app = express()
+dotenv.config()
 
+mongoose
+  .connect(
+    process.env.MONGO_URI
+  )
+  .then(() => console.log('Connected to Database')).catch(err => console.log(err))
+
+const app = express();
 
 app.listen(3000, () => {
-    console.log(`Server is running on port 3000!`)
-})
+  console.log(`Server is running on port 3000!`);
+});
+
+// mongodb+srv://sarojthapa199:sarojthapa199@mern-blog.491103y.mongodb.net/?retryWrites=true&w=majority&appName=mern-blog
