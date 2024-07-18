6a363c2dbaf702f89718a40c271179c06c7368b6
Mystic999 <eyesd676@gmail.com>
2024-07-18 19:26:54 +0545

    complete create a post UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import FooterCom from './components/Footer';
 import Header from './components/Header';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import PrivateRoute from './components/PrivateRoute';
 import About from './pages/About';
+import CreatePost from './pages/CreatePost';
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
@@ -20,6 +22,9 @@ const App = () => {
         <Route path='/sign-up' element={<SignUp />} />
         <Route element={<PrivateRoute />}>
           <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path='/create-post' element={<CreatePost />} />
         </Route>
         <Route path='/projects' element={<Projects />} />
       </Routes>
