6a8388bac4e0c370adf4228b324c342cd38eb9f5
Mystic999 <eyesd676@gmail.com>
2024-07-20 20:51:08 +0545

    show the comments of a post

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,28 +1,39 @@
-import Comment from "../models/comment.model.js"
-import { errorHandler } from "../utils/error.js"
+import Comment from '../models/comment.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const createComment = async (req, res, next) => {
+  try {
+    const { content, postId, userId } = req.body;
+
+    if (userId !== req.user.id) {
+      return next(
+        errorHandler(403, 'You are not allowed to create this comment')
+      );
+    }
+
+    if (!content || content.trim().length === 0) {
+      return next(errorHandler(400, 'Comment cannot be empty.'));
+    }
+
+    const newComment = new Comment({
+      content,
+      postId,
+      userId,
+    });
+    await newComment.save();
+
+    res.status(200).json(newComment);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+export const getPostComments = async (req, res, next) => {
     try {
-        const { content, postId, userId } = req.body
-
-        if (userId !== req.user.id) {
-            return next(errorHandler(403, 'You are not allowed to create this comment'))
-        }
-
-        if (!content || content.trim().length === 0) {
-            return next(errorHandler(400, 'Comment cannot be empty.'));
-        }
-
-        const newComment = new Comment({
-            content,
-            postId,
-            userId,
-        })
-        await newComment.save()
-
-        res.status(200).json(newComment)
+        const comments = await Comment.find({ postId: req.params.postId}).sort({createdAt: -1})
+        res.status(200).json(comments)
     } catch (error) {
-        console.log(error)
         next(error)
     }
-} 
+}
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,13 +1,15 @@
 import { Alert, Button, Textarea } from 'flowbite-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Comment from './Comment';
 
 const CommentSection = ({ postId }) => {
   const { currentUser } = useSelector((state) => state.user);
 
   const [comment, setComment] = useState('');
-  const [commentError, setCommentError] = useState(null)
+  const [comments, setComments] = useState([]);
+  const [commentError, setCommentError] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -29,13 +31,29 @@ const CommentSection = ({ postId }) => {
       const data = await res.json();
       if (res.ok) {
         setComment('');
-        setCommentError(null)
+        setCommentError(null);
+        setComments([data, ...comments])
       }
     } catch (error) {
-        setCommentError(error.message)
+      setCommentError(error.message);
       console.log(error);
     }
   };
+  console.log(comments);
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const res = await fetch(`/api/comment/getPostComments/${postId}`);
+        const data = await res.json();
+        if (res.ok) {
+          setComments(data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getComments();
+  }, [postId]);
 
   return (
     <div className='max-w-2xl mx-auto w-full px-3'>
@@ -82,8 +100,27 @@ const CommentSection = ({ postId }) => {
               Submit
             </Button>
           </div>
-          {commentError && <Alert color='failure' className='mt-5'>{commentError}</Alert>}
+          {commentError && (
+            <Alert color='failure' className='mt-5'>
+              {commentError}
+            </Alert>
+          )}
         </form>
+      )}
+      {comments.length === 0 ? (
+        <p className='text-sm my-5'>No comments yet!</p>
+      ) : (
+        <>
+          <div className='text-sm my-5 flex items-center gap-1'>
+            <p>Comments</p>
+            <div className='border border-gray-400 py-1 px-2 rounded-sm'>
+              <p>{comments.length}</p>
+            </div>
+          </div>
+          {comments.map(comment => (
+            <Comment key={comment._id} comment={comment} />
+          ))}
+        </>
       )}
     </div>
   );
