ce680181a21596d0b3cabdb4344c6f9a727854c4
sarojthapa <sarojthapa199@gmail.com>
2024-09-09 22:32:17 +0545

    removed authorization necessity to get post comments

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -5,7 +5,7 @@ import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.post('/create', verifyToken, createComment);
-router.get('/getPostComments/:postId', verifyToken, getPostComments)
+router.get('/getPostComments/:postId', getPostComments)
 router.put('/likeComment/:commentId', verifyToken, likeComment)
 router.put('/editComment/:commentId', verifyToken, editComment)
 router.delete('/deleteComment/:commentId', verifyToken, deleteComment)
